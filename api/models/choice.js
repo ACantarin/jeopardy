f41eb08fbbac8ceff37e3ef38e4968b6e5049841
André Cantarin do Rosário <andrecantarin@gmail.com>
2025-10-28 13:43:40 -0300

    Associa model de respostas à model de perguntas

--- api/models/choice.js
+++ api/models/choice.js
@@ -10,7 +10,12 @@ module.exports = (sequelize, DataTypes) => {
 		 * The `models/index` file will call this method automatically.
 		 */
 		static associate(models) {
-			// define association here
+			Choice.belongsTo(models.Question, {
+				foreignKey: {
+					name: 'question_id',
+					allowNull: false
+				}
+			});
 		}
 	}
 	Choice.init({
